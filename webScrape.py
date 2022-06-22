26083471d0fd4fa64667484e960ec7ff5e53d93f
kasullivan <kasullivan@westmonroe.com>
2022-06-22 10:30:26 -0500

    reading in all discussion page urls

--- webScrape.py
+++ webScrape.py
@@ -2,17 +2,45 @@
 import requests
 from bs4 import BeautifulSoup
 
-discussion_url = "https://live.paloaltonetworks.com/t5/discussions/ct-p/members"
-discussion_page = requests.get(discussion_url)
+# Read in the URL
+#discussionHome_url = "https://live.paloaltonetworks.com/t5/discussions/ct-p/members"
 
-soup = BeautifulSoup(discussion_page.content, 'html.parser')
-print(soup.prettify)
+# Get the page content
+#discussionHome_page = requests.get(discussionHome_url)
 
-data = []
+# Use beautiful soup to parse the page
+#soup = BeautifulSoup(discussionHome_page.content, 'html.parser')
+# print(soup.prettify)
 
-discussion_elements = soup.find_all(
-    "div", class_="custom-tiled-node-navigation")
+#tag_a = soup.find_all("a")
+# tag_a.string
+# for string in soup.stripped_strings:
+#    print(repr(string))
 
-for discussion_element in discussion_elements:
-    title_element = discussion_element.find("h3", a="a")
-    print(title_element)
+
+#discussionHome_titles = soup.find_all('h3', a_='href')
+# print(discussionHome_titles)
+
+# for x in soup.find('div', class_='custom-tiled.node-navigation'):
+#   title =
+
+# Read in discussion page urls
+# Maybe make into a dictionary
+generalPost_url = "https://live.paloaltonetworks.com/t5/general-topics/bd-p/members_discuss"
+bestPractice_url = "https://live.paloaltonetworks.com/t5/best-practice-assessment/bd-p/Best_Practice_Assessment_Discussions"
+cortexXDR_url = "https://live.paloaltonetworks.com/t5/cortex-xdr-discussions/bd-p/Analytics_Discussions"
+cortexXSOAR_url = "https://live.paloaltonetworks.com/t5/cortex-xsoar-discussions/bd-p/Cortex_XSOAR_Discussions"
+customSignatures_url = "https://live.paloaltonetworks.com/t5/custom-signatures/bd-p/CustomSignatures"
+endpoint_url = "https://live.paloaltonetworks.com/t5/endpoint-traps-discussions/bd-p/Endpoint_Discussions"
+globalProtect_url = "https://live.paloaltonetworks.com/t5/globalprotect-discussions/bd-p/GlobalProtect_Discussions"
+nextGenFirewalls_url = "https://live.paloaltonetworks.com/t5/next-generation-firewall/bd-p/NGFW_Discussions"
+prismaAccessInsights_url = "https://live.paloaltonetworks.com/t5/prisma-access-insights/bd-p/Prisma_Access_Insights_Discussions"
+panorama_url = "https://live.paloaltonetworks.com/t5/panorama-discussions/bd-p/Panorama_Discussions"
+prismaAccess_url = "https://live.paloaltonetworks.com/t5/prisma-access-discussions/bd-p/Prisma_Access_Discussions"
+virusTotal_url = "https://live.paloaltonetworks.com/t5/virustotal/bd-p/VirusTotal_Discussions"
+prismaCloud_url = "https://live.paloaltonetworks.com/t5/prisma-cloud-discussions/bd-p/Prisma_Cloud_Discussions"
+threatVulnerability_url = "https://live.paloaltonetworks.com/t5/threat-vulnerability-discussions/bd-p/Threat_Discussions"
+vmPrivate_url = "https://live.paloaltonetworks.com/t5/vm-series-in-the-private-cloud/bd-p/Private_Cloud_Discussions"
+vmPublic_url = "https://live.paloaltonetworks.com/t5/vm-series-in-the-public-cloud/bd-p/AWS_Azure_Discussions"
+
+# Get each page's content
